75316f04ab2aa0ea00050219d8a1e68b1c510a15
ggordyn <ggordyn@itba.edu.ar>
2021-03-09 22:42:36 -0300

    funciones clase level

--- tp1/src/main/java/ar/edu/itba/sia/Model/Level.java
+++ tp1/src/main/java/ar/edu/itba/sia/Model/Level.java
@@ -21,6 +21,30 @@ public class Level {
     //funcion que calcule posibles acciones dependiendo del estado (moverse, empujar caja)
 
     //funcion que se fije si teniendo un estado se gano (estan las cajas en las goals)
+    public boolean hasWon(State s){
+        for(Position box : s.boxes){
+            if(!goals.contains(box))
+                return false;       //si hay una caja que no esta en goal no se ganó
+        }
+        return true;
+    }
 
     //funcion que teniendo un estado se fije si hay deadlock
+    public boolean testDeadlock(State s){
+        for(Position box : s.boxes){
+            int x = box.x;
+            int y = box.y;
+
+            /*
+            habria que checkear cada posible deadlock:
+            top left
+            top right
+            bottom left
+            bottom right
+            no se si hay otras
+
+             */
+        }
+        return false;
+    }
 }
