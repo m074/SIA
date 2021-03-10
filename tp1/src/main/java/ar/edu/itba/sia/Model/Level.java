33d65e6638ce31d6329d508ccc245beffebb7124
ggordyn <ggordyn@itba.edu.ar>
2021-03-09 22:30:06 -0300

    clase level que tendra la info del nivel

--- tp1/src/main/java/ar/edu/itba/sia/Model/Level.java
+++ tp1/src/main/java/ar/edu/itba/sia/Model/Level.java
@@ -1,7 +1,26 @@
 package tp1.src.main.java.ar.edu.itba.sia.Model;
 
+import java.util.HashSet;
+
 public class Level {
     //clase para almacenar nivel del sokoban
     //de alguna forma habria que recibir de un archivo el mapa (con ascii quizas) y
-    //guardarse las posiciones, quizas en una matriz
+    //guardarse las posiciones
+    HashSet<Position> floor; //no se si guardar el piso vacio o no hace falta
+    HashSet<Position> goals;
+    HashSet<Position> walls;
+    State startingState;    //tendria la posicion inicial del jugador y las cajas
+
+    public Level(HashSet<Position> floor, HashSet<Position> goals, HashSet<Position> walls, State startingState) {
+        this.floor = floor;
+        this.goals = goals;
+        this.walls = walls;
+        this.startingState = startingState;
+    }
+
+    //funcion que calcule posibles acciones dependiendo del estado (moverse, empujar caja)
+
+    //funcion que se fije si teniendo un estado se gano (estan las cajas en las goals)
+
+    //funcion que teniendo un estado se fije si hay deadlock
 }
