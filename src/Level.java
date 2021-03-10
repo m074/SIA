48bf633695e6ad734e7978e3a9bc2fb985f26887
ggordyn <ggordyn@itba.edu.ar>
2021-03-09 21:32:40 -0300

    más clases bases

---
+++ src/Level.java
@@ -0,0 +1,7 @@
+package src;
+
+public class Level {
+    //clase para almacenar nivel del sokoban
+    //de alguna forma habria que recibir de un archivo el mapa (con ascii quizas) y
+    //guardarse las posiciones, quizas en una matriz
+}
--- src/State.java
+++ src/State.java
@@ -1,9 +1,31 @@
 package src;
 
+import java.util.HashSet;
+import java.util.Objects;
+
 public class State {
     //habria que saber la ubicacion del jugador y de las cajas para saber el estado
     //considerando coordenadas (x,y)
-    //una variable con la coordenada para el jugador
-    //una o mas variables para las coordenadas para las cajas
+    Position player;
+    //una o mas variables para las coordenadas para las cajas (unicas)
+    HashSet<Position> boxes;
 
+    public State(Position player, HashSet<Position> boxes) {
+        this.player = player;
+        this.boxes = boxes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        State state = (State) o;
+        return Objects.equals(player, state.player) &&
+                Objects.equals(boxes, state.boxes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(player, boxes);
+    }
 }
