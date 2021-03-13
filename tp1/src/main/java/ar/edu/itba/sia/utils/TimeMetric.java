e273d11d3506e4c0dc3d7a4f81b443e1a62e0174
Matias Ota <mepuedeayudar@hotmail.com>
2021-03-12 22:52:13 -0300

    Arreglado estilo de BFS y se agrega el timer

---
+++ tp1/src/main/java/ar/edu/itba/sia/utils/TimeMetric.java
@@ -0,0 +1,35 @@
+package ar.edu.itba.sia.utils;
+
+
+public class TimeMetric {
+
+    private Long start;
+    private Long stop;
+
+    public TimeMetric() {
+        this.start = null;
+        this.stop = null;
+    }
+
+
+    public void startTime() {
+        if(this.start != null) {
+            return;
+        }
+        this.start = System.currentTimeMillis();
+    }
+
+    public void stopTime() {
+        if(this.start == null || this.stop != null) {
+            return;
+        }
+        this.stop = System.currentTimeMillis();
+    }
+
+    public long getTime() {
+        if(this.start == null || this.stop == null) {
+            return 0;
+        }
+        return this.stop - this.start;
+    }
+}
