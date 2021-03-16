a74ce89bc1ee82631b3bd7ed4769a08d6a102284
Matias Ota <mepuedeayudar@hotmail.com>
2021-03-16 19:33:49 -0300

    Modificado las heuristicas

--- tp1/src/main/java/ar/edu/itba/sia/Algorithm/Heuristic/GoalDistanceHeuristic.java
+++ tp1/src/main/java/ar/edu/itba/sia/Algorithm/Heuristic/GoalDistanceHeuristic.java
@@ -1,15 +1,14 @@
 package ar.edu.itba.sia.Algorithm.Heuristic;
 
 import ar.edu.itba.sia.Model.Level;
-import ar.edu.itba.sia.Model.Node;
 import ar.edu.itba.sia.Model.Position;
 import ar.edu.itba.sia.Model.State;
 
-public class BoxDistanceHeuristic extends Heuristic {
+public class GoalDistanceHeuristic extends Heuristic {
     private Level level;
 
 
-    public BoxDistanceHeuristic(Level level){
+    public GoalDistanceHeuristic(Level level){
         this.level = level;
     }
 
@@ -17,12 +16,12 @@ public class BoxDistanceHeuristic extends Heuristic {
     @Override
     public int calculate_h(State state) {
         int total = 0;
-        for (Position box_position: state.box_positions) {
+        for (Position goal_postion: level.goals) {
             int min = Integer.MAX_VALUE;
-            for (Position goal_postion: level.goals) {
-
+            for (Position box_position: state.box_positions) {
                 int distance = box_position.distance(goal_postion);
                 if(min > distance){
+//                    System.out.println(distance);
                     min = distance;
                 }
             }
