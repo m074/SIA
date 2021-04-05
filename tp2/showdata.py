7b38c5874aae71664d28d015cfe43abd9e5d4098
santiagoburgos <sburgos@itba.edu.ar>
2021-04-05 16:28:52 -0300

    showdata

--- tp2/showdata.py
+++ tp2/showdata.py
@@ -10,7 +10,8 @@ x_vals = []
 y_vals = []
 
 index = count()
-plt.figure(figsize=(10, 10), dpi=150)
+
+plt.figure(figsize=(8, 8), dpi=100)
 
 def animate(i):
     data = pd.read_csv('data.csv')
@@ -20,20 +21,20 @@ def animate(i):
     y3 = data['mean']
     y4 = data['std']
 
-    # plt.cla()
+    plt.cla()
 
     plt.plot(x, y1, label='máximo', color='C1')
     plt.plot(x, y2, label='mínimo', color='C3')
     plt.errorbar(x, y3, y4, color='C2', ecolor='wheat', elinewidth=5, label='media')
 
     plt.legend(loc='lower right')
-    # plt.tight_layout()
+    plt.xlabel('Generación')
+    #plt.ylim(bottom=0,top=35)
+    plt.ylabel('Fitness')
+
+ani = FuncAnimation(plt.gcf(), animate, interval=100) #0.1 seg
 
 
-# ani = FuncAnimation(plt.gcf(), animate, interval=1000) #0.1 seg
-animate(1)
-plt.xlabel('Generación')
-plt.ylim(bottom=0,top=35)
-plt.ylabel('Fitness')
-# plt.tight_layout()
-plt.show()
+
+plt.tight_layout()
+plt.show() 
