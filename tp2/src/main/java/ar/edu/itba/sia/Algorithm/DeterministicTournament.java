8c37b083d872d556b3caa67d15a16fb3886d7a32
santiagoburgos <sburgos@itba.edu.ar>
2021-04-06 09:15:10 -0300

    import

--- tp2/src/main/java/ar/edu/itba/sia/Algorithm/DeterministicTournament.java
+++ tp2/src/main/java/ar/edu/itba/sia/Algorithm/DeterministicTournament.java
@@ -2,6 +2,7 @@ package ar.edu.itba.sia.Algorithm;
 
 import ar.edu.itba.sia.Model.Character;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Random;
