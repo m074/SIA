6c681a327e46cfe12f3ea4e1c8c82f61412522c7
Matias Ota <mepuedeayudar@hotmail.com>
2021-05-15 17:36:53 -0300

    Agregado los metodos para las matrices

--- tp4/src/main/java/ar/edu/itba/sia/utils/Utils.java
+++ tp4/src/main/java/ar/edu/itba/sia/utils/Utils.java
@@ -15,4 +15,74 @@ public class Utils {
             array[i] = temp;
         }
     }
+
+    public static double[][] dotArray(double[][] A, double[][] B) {
+
+        int aRows = A.length;
+        int aColumns = A[0].length;
+        int bRows = B.length;
+        int bColumns = B[0].length;
+
+        if (aColumns != bRows) {
+            throw new IllegalArgumentException("A:Rows: " + aColumns + " did not match B:Columns " + bRows + ".");
+        }
+
+        double[][] C = new double[aRows][bColumns];
+        for (int i = 0; i < aRows; i++) {
+            for (int j = 0; j < bColumns; j++) {
+                C[i][j] = 0.00000;
+            }
+        }
+
+        for (int i = 0; i < aRows; i++) { // aRow
+            for (int j = 0; j < bColumns; j++) { // bColumn
+                for (int k = 0; k < aColumns; k++) { // aColumn
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        return C;
+    }
+
+    public static void div_Array(double[][] A, double scalar) {
+
+        int aRows = A.length;
+        int aColumns = A[0].length;
+
+        for (int i = 0; i < aRows; i++) {
+            for (int j = 0; j < aColumns; j++) {
+                A[i][j] = A[i][j] / scalar;
+            }
+        }
+
+    }
+
+    public static void fill_diagonalArray(double[][] A, double value) {
+
+        int aRows = A.length;
+        int aColumns = A[0].length;
+
+        for (int i = 0; i < aRows; i++) {
+            for (int j = 0; j < aColumns; j++) {
+                if(i==j){
+                    A[i][j] = value;
+                }
+            }
+        }
+
+    }
+
+    public static double[][] transposeArray(double[][] A, double[][] B) {
+
+        int aRows = A.length;
+        int aColumns = A[0].length;
+
+        double[][] C = new double[aColumns][aRows];
+        for (int i = 0; i < aRows; i++) {
+            for (int j = 0; j < aColumns; j++) {
+                C[j][i] = A[i][j];
+            }
+        }
+        return C;
+    }
 }
