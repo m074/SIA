1603eee35879ae9184e81feea4cb6da16be24202
santiagoburgos <sburgos@itba.edu.ar>
2021-05-18 09:23:34 -0300

    set orthogonality

--- tp4/src/main/java/ar/edu/itba/sia/Ej2.java
+++ tp4/src/main/java/ar/edu/itba/sia/Ej2.java
@@ -19,19 +19,54 @@ public class Ej2 {
         int[] letterX = {1,-1,-1,-1,1,-1,1,-1,1,-1,-1,-1,1,-1,-1,-1,1,-1,1,-1,1,-1,-1,-1,1};
         int[] letterZ = {1,1,1,1,1,-1,-1,-1,1,-1,-1,-1,1,-1,-1,-1,1,-1,-1,-1,1,1,1,1,1};
         int[] letterF = {1,1,1,1,1,1,-1,-1,-1,-1,1,1,1,-1,-1,1,-1,-1,-1,-1,1,-1,-1,-1,-1};
+        int[] letterQ = {-1,1,1,1,-1,1,-1,-1,-1,1,1,-1,-1,-1,1,1,-1,-1,1,1,-1,1,1,-1,1};
+        int[] letterD = {1,1,1,1,-1,1,-1,-1,-1,1,1,-1,-1,-1,1,1,-1,-1,-1,1,1,1,1,1,-1};
+        int[] letterV = {1,-1,-1,-1,1,1,-1,-1,-1,1,1,-1,-1,-1,1,-1,1,-1,1,-1,-1,-1,1,-1,-1};
+        int[] letterW = {1,-1,1,-1,1,1,-1,1,-1,1,1,-1,1,-1,1,1,1,-1,1,1,1,-1,-1,-1,1};
+        int[] letterI = {1,1,1,1,1,-1,-1,1,-1,-1,-1,-1,1,-1,-1,-1,-1,1,-1,-1,1,1,1,1,1};
+
+       // printLetter(letterI);
 
 
         List<int[]> patterns = new ArrayList<>();
-        patterns.add(letterJ);
-        patterns.add(letterT);
-        patterns.add(letterO);
-        patterns.add(letterR);
 
-        //printLetter(letterF);
+        patterns.add(letterZ);
+        patterns.add(letterM);
+        patterns.add(letterR);
+        patterns.add(letterO);
+
+
+        //ZMFO 14
+        //ZJRO 20
+        //OTVF 18
+
+        //JTOR 20
+        //XCOF 48
+
+        //ZCOF 46
+        //ZCOX 54
+
+        //CQOD 90
+        
+        int or= 0;
+        for(int a=0; a<patterns.size()-1; a++){
+
+            for(int b=a+1; b<patterns.size(); b++){
+                int s= 0;
+                for(int p=0; p<patterns.get(0).length; p++){
+                  s += patterns.get(a)[p] * patterns.get(b)[p];
+                }
+                if(s<0)
+                    s = s*-1;
+                or += s;
+            }
+
+        }
+        System.out.println(or);
 
 
         Hopfield hopfield = new Hopfield(patterns);
-        hopfield.consult(addNoise(letterT,20));
+        hopfield.consult(addNoise(letterM,30));
 
     }
 
